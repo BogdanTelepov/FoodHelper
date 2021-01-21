4fbf00f15b0782e97d7dd5837d530b356b179d45
BogdanTelepov <mr.closer95@yandex.ru>
2021-01-21 19:12:04 +0600

    added recipes view model

--- app/src/main/java/com/app/RemoteDataSource.kt
+++
@@ -1,6 +0,0 @@
-package com.app
-
-class RemoteDataSource() {
-
-
-}
---
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -0,0 +1,20 @@
+package com.example.foody.util
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "43b55c643fe7427a956d1c4d15933e65"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+    }
+
+}
