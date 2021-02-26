a2ccbcaf1f7cc278d82e613c081e7b493c0cddb4
BogdanTelepov <mr.closer95@yandex.ru>
2021-02-26 13:49:31 +0600

    added binding adapters

---
+++ app/src/main/java/com/example/foody/bindingadapters/RecipesBinding.kt
@@ -0,0 +1,57 @@
+package com.example.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.foody.data.database.RecipeEntity
+import com.example.foody.models.FoodRecipe
+import com.example.foody.util.NetworkResult
+
+class RecipesBinding {
+
+    companion object {
+
+        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+        @JvmStatic
+        fun errorImageViewVisibility(
+            imageView: ImageView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipeEntity>?
+        ) {
+
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                imageView.visibility = View.VISIBLE
+            } else if (apiResponse is NetworkResult.Loading) {
+                imageView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                imageView.visibility = View.INVISIBLE
+            }
+
+        }
+
+
+        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
+        @JvmStatic
+        fun errorTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipeEntity>?
+        ) {
+
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString().trim()
+            } else if (apiResponse is NetworkResult.Loading) {
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
+            }
+
+        }
+
+
+    }
+
+
+}
